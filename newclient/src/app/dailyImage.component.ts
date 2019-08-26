5598eef836bd2dc853ece73e5d3d834914212978
Matthew Bonig <matthew.bonig@gmail.com>
2019-08-25 19:40:14 -0600

    adding video

--- newclient/src/app/dailyImage.component.ts
+++ newclient/src/app/dailyImage.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ImageService } from './image.service';
 
+const ONE_HUNDRED_SECONDS = 100000;
 @Component({
     selector: 'iotd',
-    template: `<img [src]="(source | async)?.url"/>`,
+    template: `<div *ngIf="source$|async as source">
+    <img *ngIf="!source.url.endsWith('.mp4')" [src]="source.url"/>
+    <video #video *ngIf="source.url.endsWith('.mp4')" loop="" class="" style="max-width: 100%; max-height: 440px;" autoplay="">
+    <source type="video/mp4" [src]="source.url"></video>
+    </div>
+    `,
     providers: [ImageService],
     styles: [`
        :host{
@@ -19,11 +25,24 @@ import { ImageService } from './image.service';
 })
 
 export class DailyImage implements OnInit {
-    constructor(private imageService: ImageService){
+    source$: any = Promise.resolve({ url: "https://i.imgur.com/gH1RWUd.jpg" });
+    constructor(private imageService: ImageService) {
 
     }
-    async ngOnInit(){
-        this.source = await this.imageService.getImage();
+
+    @ViewChild('video', { static: false }) video: ElementRef;
+
+    async ngOnInit() {
+        setInterval(async () => {
+
+            this.source$ = await this.imageService.getImage();
+        }, ONE_HUNDRED_SECONDS);
+        setInterval(() => {
+            if (this.video.nativeElement.paused) {
+                this.video.nativeElement.play();
+
+            }
+        }, 1000);
+        this.source$ = await this.imageService.getImage();
     }
-    source : any = Promise.resolve({url: "https://i.imgur.com/gH1RWUd.jpg"});
 }
